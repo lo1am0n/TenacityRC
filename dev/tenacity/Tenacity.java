06779b147dd09334e754db9bd27fe997eed1d67b
Lolamon <aciditysolutions@null.net>
2024-09-18 20:26:03 -0400

    Update Tenacity.java

--- dev/tenacity/Tenacity.java
+++ dev/tenacity/Tenacity.java
@@ -36,7 +36,7 @@ public class Tenacity implements Utils {
     public static final Tenacity INSTANCE = new Tenacity();
 
     public static final String NAME = "Tela";
-    public static final String VERSION = "5.1";
+    public static final String VERSION = "1.0.0";
     public static final ReleaseType RELEASE = ReleaseType.DEV;
     public static final Logger LOGGER = LogManager.getLogger(NAME);
     public static final File DIRECTORY = new File(mc.mcDataDir, "Tenacity");
